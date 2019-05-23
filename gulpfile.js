c9a84ae814ec8cb406950f6f211b6f84193e3b47
svyatoslav <svyatoslav.gryadyuga@gmail.com>
2019-05-24 01:47:31 +0300

    Сделал спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,10 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
+var rename = require("gulp-rename");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -58,4 +62,30 @@ gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
+});
+
+
+gulp.task("sprite", function () {
+  return gulp.src(["source/img/icon-cart.svg",
+    "source/img/icon-search.svg",
+    "source/img/icon-fb.svg",
+    "source/img/icon-insta.svg",
+    "source/img/icon-twitter.svg",
+    "source/img/icon-phone.svg",
+    "source/img/icon-mail.svg",
+    "source/img/icon-htmlacademy.svg",
+    "source/img/logo-footer.svg"])
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
 });
